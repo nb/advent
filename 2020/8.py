aceeb7094acd2912cbc95e6bb866269063ad5c8c
Nikolay Bachiyski <nb@nikolay.bg>
2020-12-08 11:36:46 +0200

    Task 8.2
    And little bit of refactoring

--- 2020/8.py
+++ 2020/8.py
@@ -1,20 +1,36 @@
-for f in ('sample8.txt', 'input8.txt'):
-    lines = list(open(f).readlines())
+def replaced(ops):
+    for i, op in enumerate(ops):
+        if op['op'] == 'nop':
+            yield i, 'jmp'
+        if op['op'] == 'jmp':
+            yield i, 'nop'
+
+
+def execute(ops, index=None, override=None):
     acc = 0
     i = 0
     visited = set()
-    while i < len(lines):
-        line = lines[i]
+    while i < len(ops):
+        op = ops[i] if i != index else {'op': override, 'num': ops[i]['num']}
         if i in visited:
-            print(acc)
-            break
+            return (acc, False)
         visited.add(i)
-        op, num = line.strip().split()
-        num = int(num)
-        print(op, num, acc)
-        if op == 'acc':
-            acc = acc + num
-        if op == 'jmp':
-            i = i + num
+        if op['op'] == 'acc':
+            acc = acc + op['num']
+        if op['op'] == 'jmp':
+            i = i + op['num']
         else:
             i = i + 1
+    return (acc, True)
+
+
+for f in ('sample8.txt', 'input8.txt'):
+    ops = [{'op': line.split()[0], 'num': int(line.split()[1])}
+           for line in open(f).readlines()]
+    print(f)
+    print(execute(ops))
+    for i, override in replaced(ops):
+        (acc, success) = execute(ops, i, override)
+        if success:
+            print(acc)
+            break
